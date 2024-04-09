4557dfd928472b697ef8d07a0a3258737f857435
Deepti <2021399693.deepti@ug.sharda.ac.in>
2024-04-09 19:02:29 +0530

    details page

--- src/components/aboutKnow.js
+++ src/components/aboutKnow.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-const aboutKnow = () => {
+const AboutKnow = () => {
   return (
-    <h1>know us</h1>
+   <div className='headingAbout'>
+    <h1>About Us</h1>
+    <p>Organic food refers to food products that are produced, processed, and packaged without the use of synthetic pesticides, herbicides, fertilizers, genetically modified organisms (GMOs), antibiotics, growth hormones, or other artificial additives. Instead, organic farming practices rely on natural methods and techniques to promote soil health, biodiversity, and environmental sustainability.
+    </p>
+     <div className='container'>
+      <section className='about1'>
+        <div className='about-image'>
+          <img src="/"/>
+        </div>
+        <dvi className='about-content'>
+          <h2>Warm embrace in the basket</h2>
+          <p> organic food is characterized by its focus on natural and sustainable farming practices, environmental stewardship, and the avoidance of synthetic chemicals. Consumers who choose organic products often do so for reasons related to health, environmental concerns, and ethical considerations regarding food production.
+    </p>
+        </dvi>
+      </section>
+
+     </div>
+   </div>
   )
 }
 
-export default aboutKnow;
+export default AboutKnow;
--- src/components/itemDetail.js
+++ src/components/itemDetail.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import axios from "axios";
+import React,{useState,useEffect} from 'react ';
+import { useParams } from "react-router-dom";
 
-const itemDetail = () => {
-  return (
-    <div>itemDetail</div>
-  )
-}
+ const UseProductView=()=>{
+  const{id:productId}=useParams;
+  const{product,setProduct}=useParams;
+  const{selectQuantity,setSelectedQuantity}=useParams;
 
-export default itemDetail
+   const handleQunatityChange =({target:{value}})=>{
+    setSelectedQuantity(value);
+   };
+ 
+   useEffect(()=>{
+    const fetchCategories=async()=>{
+      try{
+        const{
+           data:{data},
+          } =await axios.get('htpp://localhost:3000/${productId}?populate=*');
+          setProduct(data);
+      }
+      catch (error){
+        console.log({ error});
+      }
+     };
+    if(productId){
+      fetchCategories()
+    }
+   },[productId]);
+
+
+   return
+   {
+    product,
+    selectQuantity,
+    handleQunatityChange,
+   };
+};
+export default UseProductView;
